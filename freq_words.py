215136a8ad906538572a3ba2728148901dbf0bea
Ispanicus <alanispani@users.noreply.github.com>
2020-02-20 13:48:09 +0100

    Pickles, ML and automation scripts
    Added pickled objects for all frequent word lists and a script for creating these objects. Added a script for doing the Naive bayes as well.

--- freq_words.py
+++ freq_words.py
@@ -2,37 +2,44 @@ import nltk
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import pickle
 
 def freq_words(essay_list):
     stop_words = stopwords.words('english')
     porter = PorterStemmer()
-    all_words = []
-    essay_lister = []
     data = pd.read_csv('cleanerstill.csv', sep=";")
+    every_word = []
     
     for es in essay_list:
-        essays = [e for e in data[es]]
+        all_words = []
+        essays = [e for e in data[es][:30000]]
         
         for i, essay in enumerate(essays):
             tmp = []
             tmp_list = []
             if type(essay) != float:
-                splt = essay.split()
-                tmp.extend([porter.stem(w) for w in splt if not w in stop_words])
+                tmp.extend([porter.stem(w) for w in essay.split() if not w in stop_words])
                 for w in tmp:
-                    if "'" in w:
-                        tmp_list.extend(w.split("'"))
-                    else:
-                        tmp_list.append(w)
+                    splt = w.split("'")
+                    for s in splt:
+                        if not s.isdigit() and s not in stop_words:
+                            tmp_list.append(s)
                 #essay_list.append((data[classifier][i],[word for word in tmp_list if word]))
                 all_words.extend(tmp_list)
                 
         freq_words = nltk.FreqDist(w for w in all_words)
-        word_features = list(freq_words)[:3000]
+        word_features = list(freq_words)[:1500]
         
-        with open(f"{es}_freq_words.txt", 'w') as file:
-            for word in word_features:
-                file.write(word+'\n')
+        with open(f"{es}_freq_words", 'wb') as file:
+            pickle.dump(freq_words, file)
+        
+        #list of ALL words throughout all essays
+        every_word.extend(all_words)
+        
+    freq_all = nltk.FreqDist(w for w in all_words)
+    all_features = list(freq_all)[:1500]
+    with open("all_freq_words", 'wb') as file:
+        pickle.dump(freq_all, file)
         
 def main():
     freq_words(['essay0','essay4','essay6','essay7','essay8'])
