8c86d82c01d0137b7f8f27e099e60b0cc7a37f1f
Ispanicus <alanispani@gmail.com>
2020-02-24 13:05:29 +0100

    Update okcupid.py
    Trying to remove "nan" from classifiers where there is no information

--- okcupid.py
+++ okcupid.py
@@ -8,13 +8,23 @@ from random import shuffle
 ngram = int(sys.argv[1])
 essay = sys.argv[2]
 classifier = sys.argv[3]
+all = sys.argv[4]
 
 if ngram == 0:
-    infile = open(f"Data/{essay}_freq_words", 'rb')
+    if all:
+        infile = open(f"Data/all_freq_words", 'rb')
+    else:
+        infile = open(f"Data/{essay}_freq_words", 'rb')
 elif ngram == 1:
-    infile = open(f"Data/{essay}_freq_bigrams", 'rb')
+    if all:
+        infile = open(f"Data/all_freq_bigrams", 'rb')
+    else:
+        infile = open(f"Data/{essay}_freq_bigrams", 'rb')
 else:
-    infile = open(f"Data/{essay}_freq_trigrams", 'rb')
+    if all:
+        infile = open(f"Data/all_freq_trigrams", 'rb')
+    else:
+        infile = open(f"Data/{essay}_freq_trigrams", 'rb')
 freq_ngrams = pickle.load(infile)
 word_features = [w for (w, f) in freq_ngrams.most_common(2000)]
 infile.close()
@@ -37,16 +47,17 @@ def bayes(ngram, essay, classifier):
        1 = bigram
        2 = trigram
     '''
-    featuresets = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[ngram][essay] if (t and class_dic[classifier])]
+    featuresets = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[ngram][essay] if (t and (class_dic[classifier] != (' ' or False)))]
     length = len(featuresets)
     shuffle(featuresets)    
     train_set, test_set = featuresets[length//2:], featuresets[:length//2]
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     print(nltk.classify.accuracy(classifier, test_set))
-    print(classifier.show_most_informative_features(100))
+    return classifier.show_most_informative_features(100)
+    
 
 def main():
-    bayes(ngram, essay, classifier)
+    print(bayes(ngram, essay, classifier))
 
 if __name__ == '__main__':
     main()
