79d0606c0d47ad7bec095b8a5b935eb6b30fa92c
Ispanicus <alanispani@users.noreply.github.com>
2020-02-25 21:52:22 +0100

    Useless update that fixed problem with okcupid. Okcupid need to change anyways after we clean the data.

--- okcupid.py
+++ okcupid.py
@@ -2,30 +2,26 @@ import nltk
 import pandas as pd
 import pickle
 import sys
-from collections import defaultdict, Counter
+from collections import defaultdict
 from nltk.classify import NaiveBayesClassifier
 from nltk.metrics.scores import precision, recall, f_measure
 from nltk.stem.porter import PorterStemmer
-from operator import itemgetter
 from random import shuffle
 
 essay = sys.argv[1]
 classifier = sys.argv[2]
 all = sys.argv[3]
-
 if all:
     infile = open(f"Data/all_freq_ngrams", 'rb')
 else:
     infile = open(f"Data/{essay}_freq_ngrams", 'rb')
-
 freq_ngrams = pickle.load(infile)
-word_features = [w for (w, f) in freq_ngrams.most_common(5000)]
+word_features = [w for (w, f) in freq_ngrams.most_common(2000)]
 infile.close()
-
 infile = open("Data/essay_ngrams", 'rb')
 ngram_tuple = pickle.load(infile)
 '''tuple with essay_unigrams, essay_bigrams and essay_trigrams
-to access unigrams, just ngram_tuple[0]["essay0"]'''
+to access unigrams, just ngram_tuple["essay0"]'''
 infile.close()
 
 def document_features(document):
@@ -36,29 +32,14 @@ def document_features(document):
     return features
 
 def bayes(essay, classifier):
-    '''0 = unigram
-       1 = bigram
-       2 = trigram
-    '''
-    temp_featuresets = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[essay] if (t and class_dic[classifier] != False and type(class_dic[classifier]) != float)]
-    counts = Counter(clas for (text, clas) in temp_featuresets)
-    min_key, min_count = min(counts.items(), key=itemgetter(1))
-    shuffle(temp_featuresets)
-    featuresets = []
-    counter = min_count
-    for tup in temp_featuresets:
-        if tup[1] == min_key:
-            featuresets.append(tup)
-        elif counter:
-            featuresets.append(tup)
-            counter -= 1
-        else:
-            pass
+    featuresets = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[essay][:len(ngram_tuple[essay])//2] if (t and class_dic[classifier] != False and type(class_dic[classifier]) != float)]
+    featuresets2 = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[essay][len(ngram_tuple[essay])//2:] if (t and class_dic[classifier] != False and type(class_dic[classifier]) != float)]
+    featuresets.extend(featuresets2)
     length = len(featuresets)
     shuffle(featuresets)
     train_set, test_set = featuresets[length//10:], featuresets[:length//10]
     classifier = NaiveBayesClassifier.train(train_set)
-    predictions, gold_labels = defaultdict(set), defaultdict(set)
+    '''predictions, gold_labels = defaultdict(set), defaultdict(set)
     print('Accuracy:',nltk.classify.accuracy(classifier, test_set))
     for i, (features, label) in enumerate(test_set):
         predictions[classifier.classify(features)].add(i)
@@ -76,7 +57,7 @@ def bayes(essay, classifier):
     #tests = set(test_set)
     #print('Precision:',precision(trains, tests))
     #print('Recall:',recall(trains, tests))
-    #print('F-Score:',f_measure(trains, tests))
+    #print('F-Score:',f_measure(trains, tests))'''
     classifier.show_most_informative_features(100)
     return
 
