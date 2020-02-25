c910d5b05f48b373d01137ae5e8edc5db0f9f7c8
Ispanicus <alanispani@users.noreply.github.com>
2020-02-25 13:32:10 +0100

    Actually fixed the balancing now

--- okcupid.py
+++ okcupid.py
@@ -6,6 +6,7 @@ from collections import defaultdict, Counter
 from nltk.classify import NaiveBayesClassifier
 from nltk.metrics.scores import precision, recall, f_measure
 from nltk.stem.porter import PorterStemmer
+from operator import itemgetter
 from random import shuffle
 
 ngram = int(sys.argv[1])
@@ -52,15 +53,12 @@ def bayes(ngram, essay, classifier):
     '''
     temp_featuresets = [(document_features(t), class_dic[classifier]) for (t, class_dic) in ngram_tuple[ngram][essay] if (t and class_dic[classifier] != False and type(class_dic[classifier]) != float)]
     counts = Counter(clas for (text, clas) in temp_featuresets)
+    min_key, min_count = min(counts.items(), key=itemgetter(1))
     shuffle(temp_featuresets)
     featuresets = []
-    smallest = 100.000
-    for key in counts:
-        if counts[key] < smallest:
-            smallest = key
-    counter = counts[smallest]
+    counter = min_count
     for tup in temp_featuresets:
-        if tup[1] == smallest:
+        if tup[1] == min_key:
             featuresets.append(tup)
         elif counter:
             featuresets.append(tup)
@@ -71,8 +69,8 @@ def bayes(ngram, essay, classifier):
     shuffle(featuresets)
     train_set, test_set = featuresets[length//2:], featuresets[:length//2]
     classifier = NaiveBayesClassifier.train(train_set)
-    print('Accuracy:',nltk.classify.accuracy(classifier, test_set))
     predictions, gold_labels = defaultdict(set), defaultdict(set)
+    print('Accuracy:',nltk.classify.accuracy(classifier, test_set))
     for i, (features, label) in enumerate(test_set):
         predictions[classifier.classify(features)].add(i)
         gold_labels[label].add(i)
--- okcupid_diet.py
+++ okcupid_diet.py
@@ -6,6 +6,7 @@ from collections import defaultdict, Counter
 from nltk.classify import NaiveBayesClassifier
 from nltk.metrics.scores import precision, recall, f_measure
 from nltk.stem.porter import PorterStemmer
+from operator import itemgetter
 from random import shuffle
 
 ngram = int(sys.argv[1])
@@ -52,29 +53,23 @@ def bayes(ngram, essay, classifier):
     '''
     temp_featuresets = [(document_features(t), "anything" if "anything" in class_dic[classifier] else "other") for (t, class_dic) in ngram_tuple[ngram][essay] if (t and class_dic[classifier] != False)]
     counts = Counter(clas for (text, clas) in temp_featuresets)
-    print(counts)
+    min_key, min_count = min(counts.items(), key=itemgetter(1))
     shuffle(temp_featuresets)
     featuresets = []
-    smallest = 100.000
-    for key in counts:
-        if counts[key] < smallest:
-            smallest = key
-    counter = counts[smallest]
+    counter = min_count
     for tup in temp_featuresets:
-        if tup[1] == smallest:
+        if tup[1] == min_key:
             featuresets.append(tup)
         elif counter:
             featuresets.append(tup)
             counter -= 1
         else:
             pass
-
     length = len(featuresets)
     shuffle(featuresets)
     train_set, test_set = featuresets[length//2:], featuresets[:length//2]
     classifier = NaiveBayesClassifier.train(train_set)
     predictions, gold_labels = defaultdict(set), defaultdict(set)
-
     print('Accuracy:',nltk.classify.accuracy(classifier, test_set))
     for i, (features, label) in enumerate(test_set):
         predictions[classifier.classify(features)].add(i)
